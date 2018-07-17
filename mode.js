f24fcc99d9d1b7976601d5c22c4c01e4bd4b10b4
Muzaffer YILDIRIM <muzaffer@yldrmzffr.com>
2018-07-17 12:00:06 +0300

    Update mode.js

--- mode.js
+++ mode.js
@@ -10,7 +10,21 @@
  }
  function okuma() {
      document.body.style.backgroundColor = '#ece1cb';
+  document.body.style.color = '#604f31';
+  document.h1.style.color = '#604f31';
+  document.h2.style.color = '#604f31';
+  document.h3.style.color = '#604f31';
+  document.h4.style.color = '#604f31';
+  document.h5.style.color = '#604f31';
+  document.h6.style.color = '#604f31';
  }
  function beyaz() {
      document.body.style.backgroundColor = '#fff';
+  document.body.style.color = '#000';
+  document.h1.style.color = '#000';
+  document.h2.style.color = '#000';
+  document.h3.style.color = '#000';
+  document.h4.style.color = '#000';
+  document.h5.style.color = '#000';
+  document.h6.style.color = '#000';
  }
