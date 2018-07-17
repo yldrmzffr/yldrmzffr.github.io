ef0abc37cb9e1f6b5fb6cfccb8e17594c4ae65c9
Muzaffer YILDIRIM <muzaffer@yldrmzffr.com>
2018-07-17 11:57:22 +0300

    Update mode.js

--- mode.js
+++ mode.js
@@ -1,6 +1,12 @@
  function koyu() {
      document.body.style.backgroundColor = '#333';
      document.body.style.color = '#fff';
+  document.h1.style.color = '#fff';
+  document.h2.style.color = '#fff';
+  document.h3.style.color = '#fff';
+  document.h4.style.color = '#fff';
+  document.h5.style.color = '#fff';
+  document.h6.style.color = '#fff';
  }
  function okuma() {
      document.body.style.backgroundColor = '#ece1cb';
