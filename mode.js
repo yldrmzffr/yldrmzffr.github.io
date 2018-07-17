e2447fb2eb34a669cb93687ccc9eb8b1827d4bed
Muzaffer YILDIRIM <muzaffer@yldrmzffr.com>
2018-07-17 11:46:58 +0300

    Create mode.js

---
+++ mode.js
@@ -0,0 +1,9 @@
+ function koyu() {
+     document.body.style.backgroundColor = '#333';
+ }
+ function okuma() {
+     document.body.style.backgroundColor = '#ece1cb';
+ }
+ function beyaz() {
+     document.body.style.backgroundColor = '#fff';
+ }
