e72b5afe204d86a9c04d3fdc6ae976eadf52b89f
muzaffer <muzaffer@yldrmzffr.com>
2020-01-01 20:46:14 +0300

    videotf

---
+++ videood/js/myobjects.js
@@ -0,0 +1,18 @@
+const myobjects = {
+    "cell phone": {
+        "url": "https://www.hepsiburada.com/apple-iphone-6-32-gb-apple-turkiye-garantili-pm-HB000003XBYX",
+        "name": "iPhone 6"
+    },
+    "laptop": {
+        "url": "https://www8.hp.com/tr/tr/laptops/business-laptops-tablets.html",
+        "name": "HP Laptop"
+    },
+    "teddy bear": {
+        "url": "https://www.civilim.com/chicco-teddy-ayicik-6-ay-chc-600140",
+        "name": "Ayıcık"
+    },
+    "bottle": {
+        "url": "https://www.carrefoursa.com/tr/sirma-su-500-ml-p-30094486",
+        "name": "Sırma Su"
+    }
+};
---
+++ videood/js/video.js
@@ -0,0 +1,8 @@
+let v = document.getElementById("video1");
+function playVideo() {
+    v.play();
+}
+function pauseVideo() {
+    v.pause();
+    scanFrame();
+}
